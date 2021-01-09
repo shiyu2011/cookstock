162a740cf4b9310e568966bdac1a3b2e2e1a3c13
Shiyu <shiyu.xu@gmail.com>
2021-01-09 00:44:11 -0800

    restructure the files

---
+++ runBatch_cookStock_superStock.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  9 00:21:35 2021
+
+@author: sxu
+"""
+from importlib import reload # python 2.7 does not require this
+import cookStock
+reload(cookStock)
+from cookStock import *
+from get_all_tickers import get_tickers as gt
+from get_all_tickers.get_tickers import Region
+
+tickers = gt.get_tickers()
+
+filtered_by_sector = gt.get_tickers_filtered(sectors=gt.SectorConstants.ENERGY)
+filtered_by_sector
+y = batch_process(filtered_by_sector, 'Energy_superStocks_1_8_2021.json')
+y.batch_strategy()

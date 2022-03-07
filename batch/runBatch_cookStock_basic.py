891926c312bd3085f879a33cdf097199528b7a6d
Shiyu <shiyu.xu@gmail.com>
2022-03-06 20:58:50 -0800

    avoid replicated tickers

--- batch/runBatch_cookStock_basic.py
+++ batch/runBatch_cookStock_basic.py
@@ -7,16 +7,20 @@ Created on Sat Jan  9 00:20:22 2021
 """
 from importlib import reload # python 2.7 does not require this
 import sys
-sys.path.insert(0, '../src/')
+sys.path.insert(0, '/Users/sxu/deeppath/stock/cookstock/src/')
 import cookStock
 reload(cookStock)
 from cookStock import *
-from get_all_tickers import get_tickers as gt
-from get_all_tickers.get_tickers import Region
+import get_tickers
+reload(get_tickers)
+from get_tickers import *
 
-tickers = gt.get_tickers()
+filtered_by_sector = get_tickers_filtered(sectors=SectorConstants.TECH)
 
-filtered_by_sector = gt.get_tickers_filtered(sectors=gt.SectorConstants.TECH)
-filtered_by_sector
-y = batch_process(filtered_by_sector, 'Tech_basic_1_12_2021.json')
+selected = [] 
+for i in filtered_by_sector: 
+    if i not in selected: 
+        selected.append(i) 
+        
+y = batch_process(selected, 'Tech_basic_3_6_2022.json')
 y.batch_financial()
